8953ae9db3081258c4644c856739f26d12aa87a6
Hugo <hugo.cicarelli@prontmed.com>
2021-03-17 18:07:16 -0300

    exporting Avatar component

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import theme from "theme";
 
+import Avatar from "Avatar";
 import Button from "Button";
 import ButtonLink from "Button/Link";
 import Icon from "Icon";
 import InputText from "Inputs/Text";
 import Typography from "Typography";
 
-export { theme, Button, ButtonLink, Icon, InputText, Typography };
+export { theme, Avatar, Button, ButtonLink, Icon, InputText, Typography };
